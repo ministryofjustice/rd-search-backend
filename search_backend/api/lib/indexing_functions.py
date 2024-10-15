5c1b9705d49acc41586a51d2179406728ef7dbde
Chloe Pugh <chloe.pugh1@justice.gov.u>
2024-10-15 17:25:05 +0100

    add function to delete docs from index

--- search_backend/api/lib/indexing_functions.py
+++ search_backend/api/lib/indexing_functions.py
@@ -19,6 +19,7 @@ def run_indexing_pipeline(dataset, document_store: OpenSearchDocumentStore, cfg,
     This function splits the data into chunks and writes to a document store. If the 'semantic' arg
     is set to True then the chunks of text are embedded and written to a vector store.
 
+    Args
     :dataset: This is list of dictionaries containing the parsed data.
     :document_store: DocumentStore object that has been set up elsewhere
     :cfg: configuration of the pipeline, as per get_config(). Includes the name of the embedding model.
@@ -59,3 +60,26 @@ def run_indexing_pipeline(dataset, document_store: OpenSearchDocumentStore, cfg,
     # Create the document store
     print("Running indexing pipeline...")
     indexing.run({"document_splitter": {"documents": docs}})
+
+
+def delete_docs(document_ids: List[str], id_metafield: str, document_store: OpenSearchDocumentStore):
+    """
+    Wrapper to show how to remove documents from the OpenSearch index.
+
+    Args
+    :document_ids: List of document ids to delete. These are IDs created by Haystack/OpenSearch.
+    :id_metafield: The name of the field in the docstore metadata containing identifiers to use
+        to select docs for deletion.
+    :document_store: OpenSearchDocumentStore object that has been set up elsewhere.
+    """
+
+    # Match up a list of values in a particular field to IDs recognised by the docstore
+    filters = {
+        "field": f"meta.{id_metafield}",
+        "operator": "in",
+        "value": document_ids,
+    }
+    results = document_store.filter_documents(filters=filters)
+    document_ids = {result.meta['_id'] for result in results['documents']}
+
+    document_store.delete_documents(document_ids)
