8debd2f04a99023368dc0a04a67e1b3b9aa24d50
Chloe Pugh <chloe.pugh1@justice.gov.u>
2024-11-14 17:09:42 +0000

    updates

--- tests/search_test.py
+++ tests/search_test.py
@@ -104,7 +104,7 @@ class TestSearch(unittest.TestCase):
 
     def test_hybrid_search_threshold_filter(self):
         """
-        Test that only search results with a score over a defined threshold are returned.
+        Test that only search results with a score over a defined threshold are returned by the hybrid search.
         """
 
         # Create a fresh mock pipeline
@@ -118,7 +118,8 @@ class TestSearch(unittest.TestCase):
         hybrid_pipeline = RetrievalPipeline(
             self.mock_document_store, self.dense_embedding_model, self.rerank_model, retrieval=mock_pipeline
         ).setup_hybrid_pipeline()
-        # when(hybrid_pipeline).run(any(list))
+
+        # Mock the part where the retrieval pipeline is run
         when(hybrid_pipeline).run(...).thenReturn(mock_prediction)
         hybrid_pipeline_init = Search(hybrid_pipeline)
 
@@ -140,6 +141,35 @@ class TestSearch(unittest.TestCase):
         results = hybrid_pipeline_init.hybrid_search("test query", threshold=threshold)
 
         self.assertEqual(len(results), 0, f"Expected no results but got {len(results)}")
+
+
+    def test_semantic_search_threshold_filter(self):
+        """
+        Test that only search results with a score over a defined threshold are returned by the semantic search.
+        """
+
+        # Create a fresh mock pipeline
+        mock_pipeline = self.create_mock_pipeline()
+
+        # Mock the behaviour of the Search instance
+        mock_prediction = [{"content": "high score", "score": 0.8}, {"content": "low score", "score": 0.4}]
+        mock_prediction = {"ranker": {"documents": [Document(content=doc["content"], score=doc["score"]) for doc in mock_prediction]}}
+        
+        # Set up the Search instance
+        semantic_pipeline = RetrievalPipeline(
+            self.mock_document_store, self.dense_embedding_model, self.rerank_model, retrieval=mock_pipeline
+        ).setup_semantic_pipeline()
+
+        # Mock the part where the retrieval pipeline is run
+        when(semantic_pipeline).run(...).thenReturn(mock_prediction)
+        semantic_pipeline_init = Search(semantic_pipeline)
+
+        # Use a threshold where we expect 1 result
+        threshold = 0.4
+        results = semantic_pipeline_init.semantic_search("test query", threshold=threshold)
+
+        self.assertEqual(len(results), 1, f"Expected 1 result with score above 0.5 but got {len(results)}")
+        self.assertEqual(results[0].content, "high score", f"Expected content 'high score', got {results[0].content}")
 
 
     def test_search_invalid_query(self):
@@ -169,3 +199,105 @@ class TestSearch(unittest.TestCase):
         # Test with BM25 search method
         results = pipeline_init.bm25_search("  ")
         self.assertEqual(len(results), 0, f"Expected 0 results but got {len(results)}")
+
+
+    def test_hybrid_search_unexpected_object_returned(self):
+        """
+        Check the hybrid search behaviour when an unexpected object is returned by the Haystack pipeline
+        """
+
+        # Create a fresh mock pipeline
+        mock_pipeline = self.create_mock_pipeline()
+
+        # Set up the retrieval pipeline
+        retrieval_pipeline = RetrievalPipeline(
+            self.mock_document_store, self.dense_embedding_model, self.rerank_model, retrieval=mock_pipeline
+        ).setup_hybrid_pipeline()
+
+        # Mock the part where the retrieval pipeline is run and return something unexpected (None)
+        when(retrieval_pipeline).run(...).thenReturn(None)
+        results = Search(retrieval_pipeline).hybrid_search("test query")
+
+        self.assertEqual(len(results), 0, f"Expected 0 results but got {len(results)}")
+
+        # Try with something returned that isn't None
+        when(retrieval_pipeline).run(...).thenReturn({'A': 1})
+        results = Search(retrieval_pipeline).hybrid_search("test query")
+
+        # Try with something that's only partially structured correctly
+        when(retrieval_pipeline).run(...).thenReturn({'ranker': {'A': 1}})
+        results = Search(retrieval_pipeline).hybrid_search("test query")
+
+        self.assertEqual(len(results), 0, f"Expected 0 results but got {len(results)}")
+
+
+    def test_semantic_search_unexpected_object_returned(self):
+        """
+        Check the semantic search behaviour when an unexpected object is returned by the Haystack pipeline
+        """
+
+        # Create a fresh mock pipeline
+        mock_pipeline = self.create_mock_pipeline()
+
+        # Set up the Search instance
+        retrieval_pipeline = RetrievalPipeline(
+            self.mock_document_store, self.dense_embedding_model, self.rerank_model, retrieval=mock_pipeline
+        ).setup_semantic_pipeline()
+
+        # Mock the part where the retrieval pipeline is run and return something unexpected (None)
+        when(retrieval_pipeline).run(...).thenReturn(None)
+        results = Search(retrieval_pipeline).semantic_search("test query")
+
+        self.assertEqual(en(results), 0, f"Expected 0 results but got {len(results)}")
+
+        # Try with something returned that isn't None
+        when(retrieval_pipeline).run(...).thenReturn({'A': 1})
+        results = Search(retrieval_pipeline).semantic_search("test query")
+
+        self.assertEqual(len(results), 0, f"Expected 0 results but got {len(results)}")
+
+        # Try with something that's only partially structured correctly
+        when(retrieval_pipeline).run(...).thenReturn({'ranker': {'A': 1}})
+        results = Search(retrieval_pipeline).semantic_search("test query")
+
+        self.assertEqual(len(results), 0, f"Expected 0 results but got {len(results)}")
+
+
+    def test_bm25_search_unexpected_object_returned(self):
+        """
+        Check the BM25 search behaviour when an unexpected object is returned by the Haystack pipeline
+        """
+
+        # Create a fresh mock pipeline
+        mock_pipeline = self.create_mock_pipeline()
+
+        # Set up the Search instance
+        retrieval_pipeline = RetrievalPipeline(
+            self.mock_document_store, self.dense_embedding_model, self.rerank_model, retrieval=mock_pipeline
+        ).setup_bm25_pipeline()
+
+        # Mock the part where the retrieval pipeline is run and return something unexpected (None)
+        when(retrieval_pipeline).run(...).thenReturn(None)
+        results = Search(retrieval_pipeline).bm25_search("test query")
+
+        self.assertEqual(len(results), 0, f"Expected 0 results but got {len(results)}")
+
+        # Try with something returned that isn't None
+        when(retrieval_pipeline).run(...).thenReturn({'A': 1})
+        results = Search(retrieval_pipeline).bm25_search("test query")
+
+        self.assertEqual(len(results), 0, f"Expected 0 results but got {len(results)}")
+
+        # Try with something that's only partially structured correctly
+        when(retrieval_pipeline).run(...).thenReturn({'bm25_retriever': {'A': 1}})
+        results = Search(retrieval_pipeline).bm25_search("test query")
+
+        self.assertEqual(len(results), 0, f"Expected 0 results but got {len(results)}")
+
+        # Try with a correctly structured object
+        mock_prediction = [{"content": "high score", "score": 0.8}, {"content": "low score", "score": 0.4}]
+        mock_prediction = {"ranker": {"documents": [Document(content=doc["content"], score=doc["score"]) for doc in mock_prediction]}}
+        when(retrieval_pipeline).run(...).thenReturn(mock_prediction)
+        results = Search(retrieval_pipeline).bm25_search("test query")
+
+        self.assertEqual(len(results), 2, f"Expected 2 results but got {len(results)}")
