7c3dc2e85e0bbc0670f7f1ac75b95b685c1237da
Chloe Pugh <chloe.pugh1@justice.gov.u>
2024-12-13 17:10:49 +0000

    unit tests

--- search_backend/threshold_score.py
+++ search_backend/threshold_score.py
@@ -8,26 +8,16 @@ class ThresholdScore:
     A Haystack component to filter results based on a threshold match score.
     """
 
-    def __init__(
-        self,
-        score_threshold: float = 0.0 
-    ):
-        
-        self.score_threshold = score_threshold
-
-
     @component.output_types(documents=List[Document])
     def run(
         self,
         documents: List[Document],
-        score_threshold: Optional[float] = None
+        score_threshold: float = 0.0
     ):
         
         if not documents:
             return {"documents": []}
         
-        score_threshold = score_threshold or self.score_threshold
-
         if score_threshold < 0 or score_threshold > 1:
             raise ValueError(f"threshold must be between 0 and 1 (inclusive), but got {score_threshold}")
 
